1a49f8ade037eb558d3594744e949f5c9dedaaa3
Natalie Michelsen <natalie.michelsen@outlook.com>
2020-07-21 09:46:11 -0600

    server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+// require - ✓
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+// port - ✓
+const PORT = process.env.PORT || 3000;
 
+// express app - ✓
 const app = express();
 
+// middleware - ✓
 app.use(logger("dev"));
 
 app.use(compression());
@@ -15,14 +19,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose connect - ✓
+mongoose.connect(
+  process.env.MONGODB_URI || `mongodb://localhost/budget`,
+  { useNewUrlParser: true }
+);
 
-// routes
+// routes - ✓
 app.use(require("./routes/api.js"));
 
+// listening - ✓
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port http://localhost:${PORT}!`);
 });
