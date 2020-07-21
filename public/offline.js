60e78e25e0d843e1173fbf0596ac6113c9d25973
Natalie Michelsen <natalie.michelsen@outlook.com>
2020-07-21 10:06:22 -0600

    added offline.js & outlined offline, service-worker, and webmanifest

---
+++ public/offline.js
@@ -0,0 +1,20 @@
+// db variable
+let db;
+
+// index variable
+const index = indexedDB.open('budget, 1');
+
+// index events
+index.onupgradeneeded
+index.onsuccess
+index.onerror
+
+// run a check to make sure there is data in the indexedDB
+function runCheck() {
+
+}
+
+// save a record of the transaction
+function saveRecord(data) {
+
+}
---
+++ public/service-worker.js
@@ -0,0 +1,18 @@
+// files to cache
+const FILES_TO_CACHE = [
+
+]
+
+// to run app & cache
+const RUNTIME = '';
+const PRECACHE = '';
+
+// event listener for activating the event
+self.addEventListener('activate', event => {
+
+});
+
+// event listener for fetching the data
+self.addEventListener('fetch', data => {
+    
+});
